2e01ab44140bfdecdf9cf9117c7110b1a6f80f33
Protick Roy <protickrr@gmail.com>
2023-03-11 20:19:25 +0600

    Listing seeder factory and model with migration files

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,36 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all()
-    {
-        return [
-            [
-                "id" => 1,
-                "title" => "listing one",
-                "description" => "listing description lorem ipsum doler sit amet"
-            ],
-            [
-                "id" => 2,
-                "title" => "listing two",
-                "description" => "listing description lorem ipsum doler sit amet"
-            ],
-            [
-                "id" => 3,
-                "title" => "listing three",
-                "description" => "listing description lorem ipsum doler sit amet"
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-        foreach ($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-        return [];
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -6,16 +6,18 @@
 {{--<?php endforeach; ?>--}}
 
 {{-- Blade syntax / directives --}}
-<h1>{{ $heading }}</h1>
-@if(count($listings) == 0)
-    <p>No listings found !</p>
-@endif
-@foreach ($listings as $listing)
-    <h2>
-        <a href="/listings/{{$listing['id']}}">
-            {{$listing['title']}}
-        </a>
-    </h2>
-    <p> {{$listing['description']}} </p>
-@endforeach
-
+@extends('layout')
+@section('content')
+    <h1>{{ $heading }}</h1>
+    @if(count($listings) == 0)
+        <p>No listings found !</p>
+    @endif
+    @foreach ($listings as $listing)
+        <h2>
+            <a href="/listings/{{$listing['id']}}">
+                {{$listing['title']}}
+            </a>
+        </h2>
+        <p> {{$listing['description']}} </p>
+    @endforeach
+@endsection
